4d05967c3870e6a23d171357a3ce21d9e1a2ba17
Stanley Huang <huang.stanley050@gmail.com>
2020-02-19 09:54:29 +1100

    commented out webpack-dev-middleware

--- src/server/express.js
+++ src/server/express.js
@@ -1,9 +1,13 @@
 import express from "express";
 import path from "path";
-import webpack from "webpack";
-import config from "../../config/webpack.dev.js";
+//import webpack from "webpack";
+//import config from "../../config/webpack.dev.js";
+//import webpackDevMiddleware from "webpack-dev-middleware";
 
+//const compiler = webpack(config);
+//webpackDevMiddleware(compiler, config.devServer);
 const app = express();
 const staticMiddleWare = express.static("dist");
+//app.use(webpackDevMiddleware);
 app.use(staticMiddleWare);
 app.listen(8000, () => console.log("server running"));
